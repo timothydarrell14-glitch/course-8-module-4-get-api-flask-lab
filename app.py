f78b76d15c5850d79376b94016d2b758ada8c425
TimDev14 <njagi.timothy14@gmail.com>
2026-06-28 01:36:42 +0300

    update Python version to 3.14.4 in Pipfile and Pipfile.lock; implement home and products routes in app.py

--- app.py
+++ app.py
@@ -7,19 +7,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    pass  # TODO: Return a welcome message
+    return "Welcome to the Home Page"  # TODO: Return a welcome message
 
 # TODO: Implement GET /products route that returns all products or filters by category
 
-@app.route("/products")
+@app.route("/products", method=["GET"])
 def get_products():
-    pass  # TODO: Return all products or filter by ?category=
+    # TODO: Return all products or filter by ?category=
+    global data 
+    category = request.args.get("category")
+    if category:
+        filtered = [item for item in data if item["category"] == category]
+        return jsonify(filtered), 200
+    return jsonify(data), 200
 
 # TODO: Implement GET /products/<id> route that returns a specific product by ID or 404
 
 @app.route("/products/<int:id>")
 def get_product_by_id(id):
-    pass  # TODO: Return product by ID or 404
+    # TODO: Return product by ID or 404
+    for p in data:
+       if p["id"] == id:
+          return jsonify(p), 200
+    return jsonify({"message": "Product not found"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
